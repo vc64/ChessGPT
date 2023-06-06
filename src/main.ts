e60192dd96f7189347d257a1dfeecfd9d0b39bd1
vc64 <vchen2022@spyponders.com>
2023-06-05 23:20:10 -0400

    first working version

---
+++ src/main.ts
@@ -0,0 +1,103 @@
+import { h, init, VNode, classModule, attributesModule, eventListenersModule } from 'snabbdom';
+import { Api } from 'chessground/api';
+import page from 'page';
+import { Unit, list } from './units/unit';
+
+export function run(element: Element) {
+  const patch = init([classModule, attributesModule, eventListenersModule]);
+
+  const lastZoom = parseFloat(localStorage.getItem('lichess-dev.cge.zoom')!) || 100;
+
+  let unit: Unit, cg: Api, vnode: VNode;
+
+  function redraw() {
+    vnode = patch(vnode || element, render());
+  }
+
+  function runUnit(vnode: VNode) {
+    const el = vnode.elm as HTMLElement;
+    el.className = 'cg-wrap';
+    cg = unit.run(el);
+    window['cg'] = cg; // for messing up with it from the browser console
+    if (lastZoom !== 100) setZoom(lastZoom);
+  }
+
+  function setZoom(zoom: number) {
+    const el = document.querySelector('.cg-wrap') as HTMLElement;
+    if (el) {
+      const px = `${(zoom / 100) * 320}px`;
+      el.style.width = px;
+      el.style.height = px;
+      document.body.dispatchEvent(new Event('chessground.resize'));
+    }
+  }
+
+  function render() {
+    return h('div#chessground-examples', [
+      h(
+        'menu',
+        list.map((ex, id) => {
+          return h(
+            'a',
+            {
+              class: {
+                active: unit.name === ex.name,
+              },
+              on: { click: () => page(`/${id}`) },
+            },
+            ex.name
+          );
+        })
+      ),
+      h('section.brown.nova', [
+        h('div.cg-wrap', {
+          hook: {
+            insert: runUnit,
+            postpatch: runUnit,
+          },
+        }),
+        h('p', unit.name),
+      ]),
+      h('control', [
+        h(
+          'button',
+          {
+            on: {
+              click() {
+                cg.toggleOrientation();
+              },
+            },
+          },
+          'Toggle orientation'
+        ),
+        h('label.zoom', [
+          'Zoom',
+          h(
+            'input',
+            {
+              attrs: {
+                type: 'number',
+                value: lastZoom,
+              },
+              on: {
+                change(e) {
+                  const zoom = parseFloat((e.target as HTMLInputElement).value);
+                  localStorage.setItem('lichess-dev.cge.zoom', zoom.toString());
+                  setZoom(zoom);
+                },
+              },
+            },
+            'Toggle orientation'
+          ),
+        ]),
+      ]),
+    ]);
+  }
+
+  page({ click: false, popstate: false, dispatch: false, hashbang: true });
+  page('/:id', ctx => {
+    unit = list[parseInt(ctx.params.id) || 0];
+    redraw();
+  });
+  page(location.hash.slice(2) || '/0');
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  entry: ['./src/game.js', './src/chessboard-1.0.0/chessboard-1.0.0.js'],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -13,12 +22,20 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "assets/[name][ext]",
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
         filename: "prod-index.html",
-        template: "index.html"
+        template: "index.html",
+        favicon: "./src/img/horse.ico"
     })
-]
+  ]
 };
