7b88d5175980c92021418e2a401b1ef26d5c9499
vc64 <vchen2022@spyponders.com>
2023-06-05 23:56:44 -0400

    removing unnecessary files

--- src/game.js
+++ src/game.js
@@ -121,7 +121,7 @@ document.getElementById("resetBtn").addEventListener("click", game);
 const ground = Chessground(document.getElementById('chessground'), {viewOnly: true});
 
 const gpt_config = new Configuration({
-	apiKey: process.env.secret_key,
+	apiKey:  pk-WkiGNtobhmAHtydNHszJqnXsfyliGYdvRZRqRBudaBUGmPad,
 	basePath: "https://api.pawan.krd/v1",
 });
 
